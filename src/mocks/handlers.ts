b1bc90fe98654d7f86b55e6cc3e6328daee67f6e
Nolan Caine <nolancaine@gmail.com>
2022-03-11 13:28:53 -0700

    Add dummy code for reference

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,3 +1,21 @@
 import { graphql } from 'msw';
 
+// import { GetTagsQuery, GetTagsQueryVariables } from 'api';
+// export const handlers = [
+//   graphql.query<GetTagsQuery, GetTagsQueryVariables>(
+//     'GetTags',
+//     (_req, res, ctx) => {
+//       return res(
+//         ctx.data({
+//           __typename: 'Query',
+//           allTags: {
+//             __typename: 'TagPage',
+//             data: [{ _id: 'aoeu', name: 'test tag', __typename: 'Tag' }, null],
+//           },
+//         })
+//       );
+//     }
+//   ),
+// ];
+
 export const handlers = [];
