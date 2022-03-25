ec17f1b910c748d6eed778878f998cc5a8b0928f
Nolan Caine <nolancaine@gmail.com>
2022-03-25 16:52:03 -0600

    Add test assertions

--- src/modules/InputFieldText/InputFieldText.test.tsx
+++ src/modules/InputFieldText/InputFieldText.test.tsx
@@ -1,19 +1,47 @@
 import { getByTestId, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { useState } from 'react';
 
 import { render } from '../../test';
 import InputFieldText from '.';
 
+const ParentForm = () => {
+  const [fieldValue, setFieldValue] = useState('');
+
+  return (
+    <form onSubmit={jest.fn().mockImplementation((e) => e.preventDefault())}>
+      <InputFieldText
+        fieldName='test'
+        fieldValue={fieldValue}
+        setFieldValue={setFieldValue}
+      />
+    </form>
+  );
+};
+
 describe('InputFieldText', () => {
-  test('it renders a field name and input element by default', () => {
-    const { getByTestId, container } = render(<InputFieldText />);
+  beforeEach(() => {
+    render(<ParentForm />);
   });
-  test('it displays typed user input', () => {
-    const { getByTestId, container } = render(<InputFieldText />);
+
+  test.skip('it renders a field name and input element by default', () => {
+    expect(HeadingEl).toBeInTheDocument();
+    expect(InputEl).toBeInTheDocument();
   });
-  test('it displays an error message upon invalid input', () => {
-    const { getByTestId, container } = render(<InputFieldText />);
+
+  test.skip('it displays typed user input', () => {
+    expect(InputEl.value).toBe('');
   });
-  test('it no longer displays an error message when input is corrected', () => {
-    const { getByTestId, container } = render(<InputFieldText />);
+
+  test.skip('it displays an error message upon invalid input', () => {
+    // type
+    expect(ErrorEl).toBeInTheDocument();
+  });
+
+  test.skip('it no longer displays an error message when input is corrected', () => {
+    // type
+    expect(ErrorEl).toBeInTheDocument();
+    // remove
+    expect(ErrorEl).not.toBeInTheDocument();
   });
 });
