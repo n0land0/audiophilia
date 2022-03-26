146c41aedb7848d571ce80e1ed5f347b06e16661
Nolan Caine <nolancaine@gmail.com>
2022-03-25 18:29:44 -0600

    Add character validation and email/tel types

--- src/modules/InputFieldText/InputFieldText.tsx
+++ src/modules/InputFieldText/InputFieldText.tsx
@@ -4,22 +4,31 @@ interface InputFieldTextProps {
   fieldValue: string;
   setFieldValue: Dispatch<SetStateAction<string>>;
   fieldName: string;
+  fieldType?: 'text' | 'tel' | 'email';
 }
 
 const InputFieldText: FC<InputFieldTextProps> = ({
   fieldValue,
   setFieldValue,
   fieldName,
+  fieldType = 'text',
 }) => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setFieldValue(event.target.value);
   };
 
+  const validChars = /^[a-zA-Z0-9- ]*$/;
+
+  const formatWarning = validChars.test(fieldValue) ? null : (
+    <p>Wrong format</p>
+  );
+
   return (
     <>
       <h6>{fieldName}</h6>
+      {formatWarning && formatWarning}
       <input
-        type='text'
+        type={fieldType}
         onChange={handleChange}
         name={fieldName}
         value={fieldValue}
