982a914a63ad54c72dc81650e81b323e1002ad30
Nolan Caine <nolancaine@gmail.com>
2022-03-25 18:29:25 -0600

    Write tests

--- src/modules/InputFieldText/InputFieldText.test.tsx
+++ src/modules/InputFieldText/InputFieldText.test.tsx
@@ -19,29 +19,54 @@ const ParentForm = () => {
   );
 };
 
+// currently triggers an error
+//  Error: Not implemented: HTMLFormElement.prototype.submit
+// since it's not related to the text fields I'm not concerned
+
 describe('InputFieldText', () => {
   beforeEach(() => {
     render(<ParentForm />);
   });
 
-  test.skip('it renders a field name and input element by default', () => {
+  test('it renders a field name and input element by default', () => {
+    const HeadingEl = screen.getByRole('heading', { name: 'test' });
+    const InputEl = screen.getByRole('textbox');
+
     expect(HeadingEl).toBeInTheDocument();
     expect(InputEl).toBeInTheDocument();
   });
 
-  test.skip('it displays typed user input', () => {
+  test('it displays typed user input', () => {
+    const InputEl = screen.getByRole('textbox');
+
     expect(InputEl.value).toBe('');
+
+    userEvent.type(InputEl, 'here is some text');
+    expect(InputEl.value).toBe('here is some text');
   });
 
-  test.skip('it displays an error message upon invalid input', () => {
-    // type
+  test('it displays an error message upon invalid input', () => {
+    const InputEl = screen.getByRole('textbox');
+
+    expect(InputEl.value).toBe('');
+
+    userEvent.type(InputEl, '$');
+
+    const ErrorEl = screen.getByText('Wrong format');
     expect(ErrorEl).toBeInTheDocument();
   });
 
-  test.skip('it no longer displays an error message when input is corrected', () => {
-    // type
+  test('it no longer displays an error message when input is corrected', () => {
+    const InputEl = screen.getByRole('textbox');
+
+    expect(InputEl.value).toBe('');
+
+    userEvent.type(InputEl, '$');
+
+    const ErrorEl = screen.getByText('Wrong format');
     expect(ErrorEl).toBeInTheDocument();
-    // remove
+
+    userEvent.clear(InputEl);
     expect(ErrorEl).not.toBeInTheDocument();
   });
 });
