93fee090309e74dd4c4fe0b66f02a8c47e7f91d6
Nolan Caine <nolancaine@gmail.com>
2022-03-27 14:34:52 -0600

    Add styles

--- src/modules/InputFieldNumber/InputFieldNumber.tsx
+++ src/modules/InputFieldNumber/InputFieldNumber.tsx
@@ -4,12 +4,14 @@ interface InputFieldNumberProps {
   fieldValue: number;
   setFieldValue: Dispatch<SetStateAction<number>>;
   fieldName: string;
+  size: 'product detail' | 'cart';
 }
 
 const InputFieldNumber: FC<InputFieldNumberProps> = ({
   fieldValue,
   setFieldValue,
   fieldName,
+  size,
 }) => {
   const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (+event.target.value >= 0) setFieldValue(+event.target.value);
@@ -19,12 +21,31 @@ const InputFieldNumber: FC<InputFieldNumberProps> = ({
   };
   const increment = () => setFieldValue(fieldValue + 1);
 
+  const moduleStyles = `${
+    size === 'cart' ? 'w-[96px] h-[32px]' : 'w-[120px] h-[48px]'
+  } flex items-center p-[15px] bg-grayMed`;
+  const inputStyles = 'flex w-full bg-transparent text-center';
+  const decButtonStyles = `text-black/25 ${
+    fieldValue > 0 ? 'hover:text-orangeBurnt' : 'cursor-not-allowed'
+  }`;
+  const incButtonStyles = 'text-black/25 hover:text-orangeBurnt';
+
   return (
-    <>
-      <button onClick={decrement}>-</button>
-      <input type='number' name={fieldName} value={fieldValue} disabled />
-      <button onClick={increment}>+</button>
-    </>
+    <div className={moduleStyles}>
+      <button className={decButtonStyles} onClick={decrement}>
+        -
+      </button>
+      <input
+        className={inputStyles}
+        type='number'
+        name={fieldName}
+        value={fieldValue}
+        disabled
+      />
+      <button className={incButtonStyles} onClick={increment}>
+        +
+      </button>
+    </div>
   );
 };
 
